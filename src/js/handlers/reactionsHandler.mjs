018294794cac0ad1d2ef0e5efb51c87e4b374476
Krihag <125972171+Krihag@users.noreply.github.com>
2024-02-04 15:07:01 +0100

    changes
    changes

--- src/js/handlers/reactionsHandler.mjs
+++ src/js/handlers/reactionsHandler.mjs
@@ -1,13 +1,19 @@
 import { API_SOCIAL_URL } from "../api/constants.mjs";
 import authHeader from "../api/authDetails/authHeader.mjs";
+import load from "../storage/load.mjs";
+import { API_KEY } from "../api/constants.mjs";
 
 export default async function reactionsHandler(id, icon) {
+  const token = load("token");
+
   icon.addEventListener("click", async (e) => {
     const endpoint = `/posts/${id}/react/😃`;
-    console.log(e.target);
 
     const response = await fetch(API_SOCIAL_URL + endpoint, {
-      headers: authHeader(),
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "X-Noroff-API-Key": API_KEY,
+      },
       method: "PUT",
     });
     console.log(response);
